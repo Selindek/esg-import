21c1e5a1cadce679f13bfa21f43adb5532aef1c9
István Rátkai <istvan@berrycloud.co.uk>
2024-01-24 17:31:19 +0000

    sss

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# esg-import
+# ESG Test Task - Import App
 
+Build the project with Maven then run it form inside an IDE or from the command line:
+java -jar target/import-0.0.1-SNAPSHOT.jar {filename} [--api.url={customer.endpoint}]
+
+filename can be absolute or relative to the current directory.
+
+There is a test.csv file in the main directory in the project which contains some basic entries
++ one bad entry with a missing Customer Ref
+
+The application sends the customer data to the api.url defined in the application.properties file,
+defaulted to http://localhost:8080/customers
+
+The esg.test.server should be launched beforehand otherwise a connection exception is thrown to the console.
+
+The api.url can be also overridden with a launch parameter: --api.url
+
+
+example:
+
+java -jar target/import-0.0.1-SNAPSHOT.jar test.csv --api.url=http://localhost:8080/customers
--- src/main/java/com/esg/test/Import.java
+++ src/main/java/com/esg/test/Import.java
@@ -6,7 +6,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -30,6 +29,15 @@ import lombok.extern.slf4j.Slf4j;
 public class Import implements ApplicationRunner{
 
     
+  private static final String POSTCODE = "Postcode";
+  private static final String COUNTRY = "Country";
+  private static final String COUNTY = "County";
+  private static final String TOWN = "Town";
+  private static final String ADDRESS_LINE2 = "Address Line2";
+  private static final String ADDRESS_LINE1 = "Address Line1";
+  private static final String CUSTOMER_NAME = "Customer Name";
+  private static final String CUSTOMER_REF = "Customer Ref";
+  
   @Value("${api.url}")
   private String apiUrl;
   
@@ -54,38 +62,40 @@ public class Import implements ApplicationRunner{
   public void run(ApplicationArguments args) throws Exception {
     
     if (args.getNonOptionArgs().isEmpty()) {
-      log.error("add full path of the csv file as a parameter");
+      log.error("Add full path of the csv file as a parameter");
       System.exit(0);
     } 
     
     var name = args.getNonOptionArgs().get(0);
 
-    //Path path = Path.of(name);
     File file = new File(name);
-
-    
     
     if(!file.exists()) {
       log.error("Cannot find file {}", file.getAbsoluteFile());
       System.exit(0);
     }
 
+    var urls = args.getOptionValues("api.url");
+    var url = (urls!= null && !urls.isEmpty())?urls.get(0) : apiUrl; 
+      
+    
+    
     log.info("processing {}...", file.getAbsoluteFile());
 
     var customers = readCsv(new FileInputStream(file));
     
-    log.info("{} entries found" , customers.size());
+    log.info("{} entries found", customers.size());
     
-    upload(customers);
+    upload(customers, url);
   }
 
   
-  private void upload(List<Customer> customers) {
+  private void upload(List<Customer> customers, String url) {
     
     
     RestTemplate restTemplate = new RestTemplate();
     
-    log.info(apiUrl);
+    log.info("connecting to {}", url);
     for(Customer customer: customers) {
       log.info("posting {}", customer.getCustomerRef());
       try {
@@ -97,7 +107,7 @@ public class Import implements ApplicationRunner{
     
   }
 
-  public static List<Customer> readCsv(InputStream is) {
+  public List<Customer> readCsv(InputStream is) {
     try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
         CSVParser csvParser = new CSVParser(fileReader,
             CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim())) {
@@ -106,14 +116,30 @@ public class Import implements ApplicationRunner{
 
       for (CSVRecord csvRecord : csvParser.getRecords()) {
         Customer customer = new Customer();
-        customer.setCustomerRef(csvRecord.get("Customer Ref"));
-        customer.setCustomerName(csvRecord.get("Customer Name"));
-        customer.setAddressLine1(csvRecord.get("Address Line1"));
-        customer.setAddressLine2(csvRecord.get("Address Line2"));
-        customer.setTown(csvRecord.get("Town"));
-        customer.setCounty(csvRecord.get("County"));
-        customer.setCountry(csvRecord.get("Country"));
-        customer.setPostcode(csvRecord.get("Postcode"));
+        if(csvRecord.isMapped(CUSTOMER_REF)) {
+          customer.setCustomerRef(csvRecord.get(CUSTOMER_REF));
+        }
+        if(csvRecord.isMapped(CUSTOMER_NAME)) {
+          customer.setCustomerName(csvRecord.get(CUSTOMER_NAME));
+        }
+        if(csvRecord.isMapped(ADDRESS_LINE1)) {
+          customer.setAddressLine1(csvRecord.get(ADDRESS_LINE1));
+        }
+        if(csvRecord.isMapped(ADDRESS_LINE2)) {
+          customer.setAddressLine2(csvRecord.get(ADDRESS_LINE2));
+        }
+        if(csvRecord.isMapped(TOWN)) {
+          customer.setTown(csvRecord.get(TOWN));
+        }
+        if(csvRecord.isMapped(COUNTY)) {
+          customer.setCounty(csvRecord.get(COUNTY));
+        }
+        if(csvRecord.isMapped(COUNTRY)) {
+          customer.setCountry(csvRecord.get(COUNTRY));
+        }
+        if(csvRecord.isMapped(POSTCODE)) {
+          customer.setPostcode(csvRecord.get(POSTCODE));
+        }
 
         customers.add(customer);
       }
